f9318ebc6005ad9ec6c7398f339814691365aec6
imufiid <imufiid98@gmail.com>
2020-12-17 09:15:21 +0700

    get data from remote(API)

---
+++ app/src/main/java/com/mufiid/dicodingbajp/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.mufiid.dicodingbajp.data.source.remote
+
+import com.mufiid.dicodingbajp.data.source.remote.response.ContentResponse
+import com.mufiid.dicodingbajp.data.source.remote.response.CourseResponse
+import com.mufiid.dicodingbajp.data.source.remote.response.ModuleResponse
+import com.mufiid.dicodingbajp.utils.JsonHelper
+
+class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
+
+    companion object {
+        @Volatile
+        private var instance: RemoteDataSource? = null
+
+        fun getInstance(helper: JsonHelper): RemoteDataSource =
+            instance ?: synchronized(this) {
+                instance ?: RemoteDataSource(helper)
+            }
+    }
+
+    fun getAllCourses(): List<CourseResponse> = jsonHelper.loadCourses()
+    fun getModules(courseId: String): List<ModuleResponse> = jsonHelper.loadModule(courseId)
+    fun getContent(moduleId: String): ContentResponse = jsonHelper.loadContent(moduleId)
+}
