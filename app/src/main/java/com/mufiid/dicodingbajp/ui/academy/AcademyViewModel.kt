d3f5d1b87b80f19f27abf8fa70581fac4d24fe21
imufiid <imufiid98@gmail.com>
2020-12-17 09:18:15 +0700

    get data view model from repository

--- app/src/main/java/com/mufiid/dicodingbajp/ui/academy/AcademyViewModel.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/academy/AcademyViewModel.kt
@@ -2,9 +2,10 @@ package com.mufiid.dicodingbajp.ui.academy
 
 import androidx.lifecycle.ViewModel
 import com.mufiid.dicodingbajp.data.CourseEntity
+import com.mufiid.dicodingbajp.data.source.AcademyRepository
 import com.mufiid.dicodingbajp.utils.DataDummy
 
-class AcademyViewModel: ViewModel() {
+class AcademyViewModel(private val academyRepository: AcademyRepository): ViewModel() {
 
-    fun getCourse(): List<CourseEntity> = DataDummy.generateDummyCourses()
+    fun getCourse(): List<CourseEntity> = academyRepository.getAllCourses()
 }
--- app/src/main/java/com/mufiid/dicodingbajp/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/bookmark/BookmarkViewModel.kt
@@ -2,9 +2,10 @@ package com.mufiid.dicodingbajp.ui.bookmark
 
 import androidx.lifecycle.ViewModel
 import com.mufiid.dicodingbajp.data.CourseEntity
+import com.mufiid.dicodingbajp.data.source.AcademyRepository
 import com.mufiid.dicodingbajp.utils.DataDummy
 
-class BookmarkViewModel : ViewModel() {
+class BookmarkViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
 
-    fun getBookmark(): List<CourseEntity> = DataDummy.generateDummyCourses()
+    fun getBookmark(): List<CourseEntity> = academyRepository.getBookmarkedCourses()
 }
