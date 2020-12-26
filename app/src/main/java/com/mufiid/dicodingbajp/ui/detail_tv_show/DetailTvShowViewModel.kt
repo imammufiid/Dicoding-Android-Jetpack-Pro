3410f94755b65678627439e44e51095890d9f060
imufiid <imufiid98@gmail.com>
2020-12-26 12:15:45 +0700

    remove import not usage

--- app/src/main/java/com/mufiid/dicodingbajp/ui/detail_tv_show/DetailTvShowViewModel.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/detail_tv_show/DetailTvShowViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.mufiid.dicodingbajp.data.TvShowEntity
 import com.mufiid.dicodingbajp.data.source.MovieRepository
-import com.mufiid.dicodingbajp.utils.DataDummy
 
 class DetailTvShowViewModel(private val movieRepository: MovieRepository) : ViewModel() {
     private lateinit var tvShowId: String
