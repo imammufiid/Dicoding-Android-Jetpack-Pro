87b0c23fd18f1943e81b6d4657124afa4e6caa7a
imufiid <imufiid98@gmail.com>
2020-12-26 12:12:41 +0700

    change var to val

--- app/src/main/java/com/mufiid/dicodingbajp/data/MovieEntity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/data/MovieEntity.kt
@@ -2,9 +2,9 @@ package com.mufiid.dicodingbajp.data
 
 data class MovieEntity (
     val movieId: String,
-    var title: String,
-    var description: String,
-    var releaseDate: String,
-    var runtime: String,
-    var imagePath: String
+    val title: String,
+    val description: String,
+    val releaseDate: String,
+    val runtime: String,
+    val imagePath: String
 )
--- app/src/main/java/com/mufiid/dicodingbajp/data/TvShowEntity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/data/TvShowEntity.kt
@@ -2,9 +2,9 @@ package com.mufiid.dicodingbajp.data
 
 data class TvShowEntity (
     val tvShowId: String,
-    var title: String,
-    var description: String,
-    var releaseDate: String,
-    var runtime: String,
-    var imagePath: String
+    val title: String,
+    val description: String,
+    val releaseDate: String,
+    val runtime: String,
+    val imagePath: String
 )
