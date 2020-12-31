b93f14361d194f9d72882ed10faf0d573321bff1
imufiid <imufiid98@gmail.com>
2020-12-31 11:30:08 +0700

    add update feature

--- app/src/main/java/com/mufiid/dicodingbajp/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/insert/NoteAddUpdateActivity.kt
@@ -1,13 +1,20 @@
 package com.mufiid.dicodingbajp.ui.insert
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.mufiid.dicodingbajp.R
 import com.mufiid.dicodingbajp.data.source.local.entity.Note
 import com.mufiid.dicodingbajp.databinding.ActivityNoteAddUpdateBinding
+import com.mufiid.dicodingbajp.helper.DateHelper
 import com.mufiid.dicodingbajp.helper.ViewModelFactory
 
-class NoteAddUpdateActivity : AppCompatActivity() {
+class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
     private var _binding: ActivityNoteAddUpdateBinding? = null
     private val binding
         get() = _binding
@@ -35,6 +42,42 @@ class NoteAddUpdateActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         viewModel = obtainViewModel(this@NoteAddUpdateActivity)
+
+        note = intent.getParcelableExtra(EXTRA_NOTE)
+        if(note != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, 0)
+            isEdit = true
+        } else {
+            note = Note()
+        }
+
+        val actionBarTitle: String
+        val btnTitle: String
+
+        if(isEdit) {
+            actionBarTitle = getString(R.string.change)
+            btnTitle = getString(R.string.update)
+
+            if(note != null){
+                note?.let {
+                    binding?.edtTitle?.setText(it.title)
+                    binding?.edtDescription?.setText(it.description)
+                }
+            }
+        } else {
+            actionBarTitle = getString(R.string.add)
+            btnTitle = getString(R.string.save)
+        }
+
+        supportActionBar?.title = actionBarTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.btnSubmit?.text = btnTitle
+
+        init()
+    }
+
+    private fun init() {
+        binding?.btnSubmit?.setOnClickListener(this)
     }
 
     override fun onDestroy() {
@@ -45,5 +88,101 @@ class NoteAddUpdateActivity : AppCompatActivity() {
     private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
         val factory = ViewModelFactory.getInstance(activity.application)
         return ViewModelProvider(activity, factory)[NoteAddUpdateViewModel::class.java]
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_submit -> {
+                val title = binding?.edtTitle?.text.toString().trim()
+                val description = binding?.edtDescription?.text.toString().trim()
+
+                if(title.isEmpty()) {
+                    binding?.edtTitle?.error = getString(R.string.empty)
+                } else if(description.isEmpty()) {
+                    binding?.edtDescription?.error = getString(R.string.empty)
+                } else {
+                    note.let {
+                        note?.title = title
+                        note?.description = description
+                    }
+
+                    val intent = Intent().apply {
+                        putExtra(EXTRA_NOTE, note)
+                        putExtra(EXTRA_POSITION, position)
+                    }
+
+                    if(isEdit) {
+                        viewModel.update(note as Note)
+                        setResult(RESULT_UPDATE, intent)
+                        finish()
+                    } else {
+                        note.let {
+                            it?.date = DateHelper.getCurrentDate()
+                        }
+
+                        viewModel.insert(note as Note)
+                        setResult(RESULT_ADD, intent)
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(isEdit) {
+            menuInflater.inflate(R.menu.menu_form, menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_delete -> {
+                showAlertDialog(ALERT_DIALOG_DELETE)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        super.onSupportNavigateUp()
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+        return true
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose: Boolean = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+
+        if(isDialogClose) {
+            dialogTitle = getString(R.string.cancel)
+            dialogMessage = getString(R.string.message_cancel)
+        } else {
+            dialogMessage = getString(R.string.message_delete)
+            dialogTitle = getString(R.string.delete)
+        }
+
+        AlertDialog.Builder(this).apply {
+            setTitle(dialogTitle)
+            setMessage(dialogMessage)
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                if(!isDialogClose) {
+                    note?.let { viewModel.delete(it) }
+                    setResult(RESULT_DELETE, Intent().putExtra(EXTRA_POSITION, position))
+                }
+                finish()
+            }
+            setNegativeButton(getString(R.string.no)) { dialog, _ ->
+                dialog.cancel()
+            }
+        }.show()
+
     }
 }
