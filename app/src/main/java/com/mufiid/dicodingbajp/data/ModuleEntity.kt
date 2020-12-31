884caa63d2c884cf36ef8b3817f0a3f61b1b821d
imufiid <imufiid98@gmail.com>
2020-12-31 14:27:48 +0700

    update entities using room

--- app/src/main/java/com/mufiid/dicodingbajp/data/ModuleEntity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/data/ModuleEntity.kt
@@ -1,11 +1,34 @@
 package com.mufiid.dicodingbajp.data
 
-data class ModuleEntity (
+import androidx.annotation.NonNull
+import androidx.room.*
+
+@Entity(
+    tableName = "moduleentities",
+    primaryKeys = ["moduleId", "courseId"],
+    foreignKeys = [ForeignKey(
+        entity = CourseEntity::class,
+        parentColumns = ["courseId"],
+        childColumns = ["courseId"]
+    )], indices = [Index(value = ["moduleId"]), Index(value = ["courseId"])]
+)
+data class ModuleEntity(
+    @NonNull
+    @ColumnInfo(name = "moduleId")
     var moduleId: String,
+
+    @ColumnInfo(name = "courseId")
     var courseId: String,
+
+    @ColumnInfo(name = "title")
     var title: String,
+
+    @ColumnInfo(name = "position")
     var position: Int,
+
+    @ColumnInfo(name = "read")
     var read: Boolean = false
 ) {
+    @Embedded
     var contentEntity: ContentEntity? = null
 }
