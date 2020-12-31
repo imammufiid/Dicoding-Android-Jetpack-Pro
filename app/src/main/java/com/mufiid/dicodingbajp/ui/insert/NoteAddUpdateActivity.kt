7476d098f3a6d6a7a384479fc652008cbcfce14b
imufiid <imufiid98@gmail.com>
2020-12-31 09:52:49 +0700

    add companion object and menghubungkan viewmodel

--- app/src/main/java/com/mufiid/dicodingbajp/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/insert/NoteAddUpdateActivity.kt
@@ -1,12 +1,49 @@
 package com.mufiid.dicodingbajp.ui.insert
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mufiid.dicodingbajp.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.mufiid.dicodingbajp.data.source.local.entity.Note
+import com.mufiid.dicodingbajp.databinding.ActivityNoteAddUpdateBinding
+import com.mufiid.dicodingbajp.helper.ViewModelFactory
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+    private var _binding: ActivityNoteAddUpdateBinding? = null
+    private val binding
+        get() = _binding
+    private lateinit var viewModel: NoteAddUpdateViewModel
+
+    private var isEdit = false
+    private var note: Note? = null
+    private var position = 0
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val REQUEST_ADD = 100
+        const val RESULT_ADD = 101
+        const val REQUEST_UPDATE = 200
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add_update)
+        _binding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        viewModel = obtainViewModel(this@NoteAddUpdateActivity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory)[NoteAddUpdateViewModel::class.java]
     }
 }
