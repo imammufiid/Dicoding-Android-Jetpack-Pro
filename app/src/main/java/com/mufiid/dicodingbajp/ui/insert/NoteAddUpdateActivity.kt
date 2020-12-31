1ebaae629cfd8c5aff32926fd6628dafa9f9022f
imufiid <imufiid98@gmail.com>
2020-12-31 09:10:34 +0700

    create NoteAddUpdateActivity.kt

---
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.mufiid.dicodingbajp.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mufiid.dicodingbajp.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
