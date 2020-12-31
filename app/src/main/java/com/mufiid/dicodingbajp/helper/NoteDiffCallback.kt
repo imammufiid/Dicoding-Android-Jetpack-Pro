4014694742177b22476c3c5c1e42f34122adb9bd
imufiid <imufiid98@gmail.com>
2020-12-31 09:43:48 +0700

    create note diff callback

---
+++ app/src/main/java/com/mufiid/dicodingbajp/helper/NoteDiffCallback.kt
@@ -0,0 +1,22 @@
+package com.mufiid.dicodingbajp.helper
+
+import androidx.recyclerview.widget.DiffUtil
+import com.mufiid.dicodingbajp.data.source.local.entity.Note
+
+class NoteDiffCallback(private val mOldNoteList: List<Note>, private val mNewNoteList: List<Note>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = mOldNoteList.size
+
+    override fun getNewListSize(): Int = mOldNoteList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return mOldNoteList[oldItemPosition].id == mNewNoteList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldEmployee = mOldNoteList[oldItemPosition]
+        val newEmployee = mNewNoteList[newItemPosition]
+
+        return oldEmployee.title == newEmployee.title && oldEmployee.description == newEmployee.description
+    }
+}
