f9834984fd235652f5c9c8ffd5927502c73bd488
imufiid <imufiid98@gmail.com>
2020-12-31 09:01:26 +0700

    create note room database

---
+++ app/src/main/java/com/mufiid/dicodingbajp/data/source/local/entity/NoteRoomDatabase.kt
@@ -0,0 +1,25 @@
+package com.mufiid.dicodingbajp.data.source.local.entity
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteRoomDatabase : RoomDatabase() {
+    abstract fun NoteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteRoomDatabase? = null
+
+        @JvmStatic
+        fun getDatabase(context: Context): NoteRoomDatabase {
+            INSTANCE ?: synchronized(NoteRoomDatabase::class.java) {
+                INSTANCE ?: Room.databaseBuilder(context.applicationContext, NoteRoomDatabase::class.java, "note_database")
+                    .build()
+            }
+            return INSTANCE as NoteRoomDatabase
+        }
+    }
+}
