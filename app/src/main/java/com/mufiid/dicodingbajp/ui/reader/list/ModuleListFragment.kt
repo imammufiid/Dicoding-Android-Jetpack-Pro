fa0c18089af6042e4088ca2b9f0af5661a666643
imufiid <imufiid98@gmail.com>
2021-01-03 20:11:27 +0700

    update list of module

--- app/src/main/java/com/mufiid/dicodingbajp/ui/reader/list/ModuleListFragment.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/reader/list/ModuleListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +16,7 @@ import com.mufiid.dicodingbajp.ui.reader.CourseReaderActivity
 import com.mufiid.dicodingbajp.ui.reader.CourseReaderCallback
 import com.mufiid.dicodingbajp.ui.reader.CourseReaderViewModel
 import com.mufiid.dicodingbajp.viewmodel.ViewModelFactory
+import com.mufiid.dicodingbajp.vo.Status
 
 
 class ModuleListFragment : Fragment(), MyAdapterClickListener {
@@ -39,10 +41,20 @@ class ModuleListFragment : Fragment(), MyAdapterClickListener {
         viewModel = ViewModelProvider(requireActivity(), factory)[CourseReaderViewModel::class.java]
         adapter = ModuleListAdapter(this)
 
-        fragmentModuleListBinding.progressBar.visibility = View.VISIBLE
-        viewModel.getModules().observe(viewLifecycleOwner, { modules ->
-            fragmentModuleListBinding.progressBar.visibility = View.GONE
-            populateRecyclerView(modules)
+        viewModel.modules.observe(viewLifecycleOwner, {
+            if(it != null) {
+                when (it.status) {
+                    Status.LOADING ->fragmentModuleListBinding.progressBar.visibility = View.VISIBLE
+                    Status.SUCCESS -> {
+                        fragmentModuleListBinding.progressBar.visibility = View.GONE
+                        populateRecyclerView(it.data as List<ModuleEntity>)
+                    }
+                    Status.ERROR -> {
+                        fragmentModuleListBinding.progressBar.visibility = View.GONE
+                        Toast.makeText(context, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         })
     }
 
