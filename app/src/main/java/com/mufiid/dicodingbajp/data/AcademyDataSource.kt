473505e60d4f795967fce0883fb54d79c85b7717
imufiid <imufiid98@gmail.com>
2021-01-03 20:04:00 +0700

    add interface AcademyDataSource

--- app/src/main/java/com/mufiid/dicodingbajp/data/AcademyDataSource.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/data/AcademyDataSource.kt
@@ -2,12 +2,16 @@ package com.mufiid.dicodingbajp.data
 
 import androidx.lifecycle.LiveData
 import com.mufiid.dicodingbajp.data.source.local.entity.CourseEntity
+import com.mufiid.dicodingbajp.data.source.local.entity.CourseWithModule
 import com.mufiid.dicodingbajp.data.source.local.entity.ModuleEntity
+import com.mufiid.dicodingbajp.vo.Resource
 
 interface AcademyDataSource {
-    fun getAllCourses(): LiveData<List<CourseEntity>>
+    fun getAllCourses(): LiveData<Resource<List<CourseEntity>>>
     fun getBookmarkedCourses(): LiveData<List<CourseEntity>>
-    fun getCourseWithModules(courseId: String): LiveData<CourseEntity>
-    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>>
-    fun getContent(courseId: String, moduleId: String): LiveData<ModuleEntity>
+    fun getCourseWithModules(courseId: String): LiveData<Resource<CourseWithModule>>
+    fun getAllModulesByCourse(courseId: String): LiveData<Resource<List<ModuleEntity>>>
+    fun getContent(moduleId: String): LiveData<Resource<ModuleEntity>>
+    fun setCourseBookmark(course: CourseEntity, state: Boolean)
+    fun setReadModule(module: ModuleEntity)
 }
