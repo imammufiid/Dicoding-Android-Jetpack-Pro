6cf8498262cc967f97e63825f266dade7dc62a51
imufiid <imufiid98@gmail.com>
2021-01-03 20:09:36 +0700

    update detail course

--- app/src/main/java/com/mufiid/dicodingbajp/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/ui/detail/DetailCourseActivity.kt
@@ -2,8 +2,12 @@ package com.mufiid.dicodingbajp.ui.detail
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,10 +20,14 @@ import com.mufiid.dicodingbajp.databinding.ActivityDetailCourseBinding
 import com.mufiid.dicodingbajp.databinding.ContentDetailCourseBinding
 import com.mufiid.dicodingbajp.ui.reader.CourseReaderActivity
 import com.mufiid.dicodingbajp.viewmodel.ViewModelFactory
+import com.mufiid.dicodingbajp.vo.Status
 
 class DetailCourseActivity : AppCompatActivity() {
 
     private lateinit var detailContentBinding: ContentDetailCourseBinding
+    private lateinit var binding: ActivityDetailCourseBinding
+    private lateinit var viewModel: DetailCourseViewModel
+    private var menu: Menu? = null
 
     companion object {
         const val EXTRA_COURSE = "extra_course"
@@ -27,34 +35,42 @@ class DetailCourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
-        detailContentBinding = activityDetailCourseBinding.detailContent
-        setContentView(activityDetailCourseBinding.root)
+        binding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = binding.detailContent
+        setContentView(binding.root)
 
-        setSupportActionBar(activityDetailCourseBinding.toolbar)
+        setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val factory = ViewModelFactory.getInstance(this)
-        val viewModel = ViewModelProvider(this, factory)[DetailCourseViewModel::class.java]
+        viewModel = ViewModelProvider(this, factory)[DetailCourseViewModel::class.java]
         val adapter = DetailCourseAdapter()
 
         val extras = intent.extras
         if(extras != null) {
             val courseId = extras.getString(EXTRA_COURSE)
             if(courseId != null) {
-                activityDetailCourseBinding.progressBar.visibility = View.VISIBLE
-                activityDetailCourseBinding.content.visibility = View.INVISIBLE
 
                 viewModel.setSelectedCourse(courseId)
-                viewModel.getModules().observe(this, { modules ->
-                    activityDetailCourseBinding.progressBar.visibility = View.GONE
-                    activityDetailCourseBinding.content.visibility = View.VISIBLE
 
-                    adapter.setModules(modules)
-                    adapter.notifyDataSetChanged()
-                })
-                viewModel.getCourse().observe(this, {
-                    populateCourse(it)
+                viewModel.courseModule.observe(this, {
+                    if(it != null) {
+                        when(it.status) {
+                            Status.LOADING -> binding.progressBar.visibility = View.VISIBLE
+                            Status.SUCCESS -> if (it.data != null) {
+                                binding.progressBar.visibility = View.GONE
+                                binding.content.visibility = View.VISIBLE
+
+                                adapter.setModules(it.data.mModules)
+                                adapter.notifyDataSetChanged()
+                                populateCourse(it.data.mCourse)
+                            }
+                            Status.ERROR -> {
+                                binding.progressBar.visibility = View.GONE
+                                Toast.makeText(this, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
                 })
 
             }
@@ -69,6 +85,47 @@ class DetailCourseActivity : AppCompatActivity() {
             addItemDecoration(dividerItemDecoration)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        this.menu = menu
+
+        viewModel.courseModule.observe(this, {
+            if(it != null) {
+                when(it.status) {
+                    Status.LOADING -> binding.progressBar.visibility = View.VISIBLE
+                    Status.SUCCESS -> if (it.data != null) {
+                        binding.progressBar.visibility = View.GONE
+                        val state = it.data.mCourse.bookmarked
+                        setBookmarkedState(state)
+                    }
+                    Status.ERROR -> {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(applicationContext, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_bookmark) {
+            viewModel.setBookmark()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBookmarkedState(state: Boolean) {
+        if(menu == null) return
+        val menuItem = menu?.findItem(R.id.action_bookmark)
+        if (state) {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmarked_white)
+        } else {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmark_white)
+        }
     }
 
     private fun populateCourse(courseEntity: CourseEntity) {
