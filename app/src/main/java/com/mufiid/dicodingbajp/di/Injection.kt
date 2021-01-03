575edc6606d5e3e6563fc2934a2058f22d970abc
imufiid <imufiid98@gmail.com>
2021-01-03 20:06:22 +0700

    update injection adding parameter local data source and app executor

--- app/src/main/java/com/mufiid/dicodingbajp/di/Injection.kt
+++ app/src/main/java/com/mufiid/dicodingbajp/di/Injection.kt
@@ -2,13 +2,20 @@ package com.mufiid.dicodingbajp.di
 
 import android.content.Context
 import com.mufiid.dicodingbajp.data.AcademyRepository
+import com.mufiid.dicodingbajp.data.source.local.LocalDataSource
+import com.mufiid.dicodingbajp.data.source.local.room.AcademyDatabase
 import com.mufiid.dicodingbajp.data.source.remote.RemoteDataSource
+import com.mufiid.dicodingbajp.utils.AppExecutor
 import com.mufiid.dicodingbajp.utils.JsonHelper
 
 object Injection {
 
     fun provideRepository(context: Context): AcademyRepository {
+        val database = AcademyDatabase.getInstance(context)
         val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
-        return AcademyRepository.getInstance(remoteDataSource)
+        val localDataSource = LocalDataSource.getInstance(database.academyDao())
+        val appExecutors = AppExecutor()
+
+        return AcademyRepository.getInstance(remoteDataSource, localDataSource, appExecutors)
     }
 }
